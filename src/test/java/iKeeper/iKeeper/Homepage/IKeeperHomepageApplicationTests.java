ff1fc07dc12575695749aeb593372e967177aaa5
cprtmchzh <bin54511563@gmail.com>
2023-11-28 11:21:30 +0900

    Feat: 첫 커밋

---
+++ src/test/java/iKeeper/iKeeper/Homepage/IKeeperHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package iKeeper.iKeeper.Homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IKeeperHomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

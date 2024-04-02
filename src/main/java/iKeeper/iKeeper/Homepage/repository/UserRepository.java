c6542852e31233f2f2c304f9acdf4a8bb90889da
cprtmchzh <bin54511563@gmail.com>
2024-04-02 09:26:52 +0900

    Feat: User 리포지토리 작성

---
+++ src/main/java/iKeeper/iKeeper/Homepage/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package iKeeper.iKeeper.Homepage.repository;
+
+import iKeeper.iKeeper.Homepage.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}

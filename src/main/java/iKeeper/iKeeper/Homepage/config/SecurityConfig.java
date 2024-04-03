2fd9dee8bcc897dfdc2642e2e861bd270c288c82
cprtmchzh <bin54511563@gmail.com>
2024-04-03 18:22:43 +0900

    Feat: 로그인 및 로그아웃 기능 추가 및 경로 설정

--- src/main/java/iKeeper/iKeeper/Homepage/config/SecurityConfig.java
+++ src/main/java/iKeeper/iKeeper/Homepage/config/SecurityConfig.java
@@ -1,27 +1,70 @@
 package iKeeper.iKeeper.Homepage.config;
 
+import iKeeper.iKeeper.Homepage.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    UserService userService;
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers("/css/**", "/js/**", "/img/**")
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                .csrf().disable();
+
+        http
+                .authorizeRequests()
+                .antMatchers("/api/users/login", "/api/users/join").permitAll()
+                .antMatchers("/").hasRole("USER")
+                .antMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated(); // 페이지 및 권한은 추후 분배 예정
+
+        http
+                .formLogin()
+                .loginPage("/api/users/login")
+                .defaultSuccessUrl("/success") // 리다이렉션 페이지는 추후 지정 예정
+                .usernameParameter("student")
+                .failureUrl("/api/users/login/error");
+
+        http
+                .logout()
+                .logoutSuccessUrl("/success")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/api/users/logout"))
+                .invalidateHttpSession(true);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
 }

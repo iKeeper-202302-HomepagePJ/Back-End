a8a28e6bd07a67f78261f01d3248b168377b5f24
cprtmchzh <bin54511563@gmail.com>
2024-04-07 18:47:23 +0900

    Fix: 회원 평가 관련 칼럼 분리

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,6 +10,7 @@ drop table if exists field_table;
 drop table if exists major_table;
 drop table if exists status_table;
 drop table if exists grade_table;
+drop table if exists score_table;
 drop table if exists category_table;
 drop table if exists headline_table;
 drop table if exists attendance_table;
@@ -57,10 +58,20 @@ CREATE TABLE grade_table
     grade_name VARCHAR(10) DEFAULT '1학년 1학차' NOT NULL
 );
 
+CREATE TABLE score_table
+(
+    score_id            TINYINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    user_score_ikeeper  TINYINT UNSIGNED,
+    user_score_field    TINYINT UNSIGNED,
+    user_score_activity TINYINT UNSIGNED,
+    user_score_etc      TINYINT UNSIGNED,
+    user_score_sum      TINYINT UNSIGNED
+);
+
 CREATE TABLE user_table
 (
     user_id              TINYINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    student_id           VARCHAR(9)  DEFAULT '00000000'         NOT NULL,
+    student_id           VARCHAR(9)                             NOT NULL,
     user_name            VARCHAR(10) DEFAULT '이름없음'             NOT NULL,
     user_role            VARCHAR(5)                             NOT NULL,
     user_profile_picture VARCHAR(100),
@@ -79,16 +90,29 @@ CREATE TABLE user_table
     FOREIGN KEY (user_status) REFERENCES status_table (status_id),
     user_grade           TINYINT UNSIGNED NOT NULL,
     FOREIGN KEY (user_grade) REFERENCES grade_table (grade_id),
-    user_password        VARCHAR(255) DEFAULT 'i-KeeperD2509'    NOT NULL,
-    user_warning         BOOLEAN     DEFAULT '0',
-    user_score_ikeeper   TINYINT UNSIGNED,
-    user_score_field     TINYINT UNSIGNED,
-    user_score_activity  TINYINT UNSIGNED,
-    user_score_etc       TINYINT UNSIGNED,
-    user_score_sum       TINYINT UNSIGNED
+    user_score           TINYINT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_score) REFERENCES score_table (score_id),
+    user_warning         BOOLEAN     DEFAULT '0'
 );
 
 /*
+
+CREATE TABLE lecture_table
+(
+    lecture_id   TINYINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    lecture_name VARCHAR(30) DEFAULT NOT NULL
+);
+
+CREATE TABLE attendance_table
+(
+    attendance_id TINYINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    user_id       TINYINT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user_table (user_id)
+        lecture_id TINYINT UNSIGNED NOT NULL,
+    FOREIGN KEY (lecture_id) REFERENCES lecture_table (lecture_id)
+        attendance_check BOOLEAN DEFAULT '1' NOT NULL
+);
+
 CREATE TABLE category_table
 (
     category_id   TINYINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
@@ -99,7 +123,7 @@ CREATE TABLE category_list_table
 (
     category_list_table_id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
     user_id                CHAR(8) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user_table (student_id),
+    FOREIGN KEY (user_id) REFERENCES user_table (user_id),
     category_id            TINYINT UNSIGNED NOT NULL,
     FOREIGN KEY (category_id) REFERENCES category_table (category_id)
 );
@@ -165,7 +189,7 @@ CREATE TABLE attendance_list_table
 CREATE TABLE keeper_information_table
 (
     keeper_information VARCHAR(10000) NULL,
-    user_authority  TINYINT UNSIGNED NOT NULL,
+    user_authority     TINYINT UNSIGNED NOT NULL,
     FOREIGN KEY (user_authority) REFERENCES authority_table (authority_id)
 );
 
@@ -183,7 +207,7 @@ CREATE TABLE keeper_award_table
     award_id       SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
     user_authority TINYINT UNSIGNED NOT NULL,
     FOREIGN KEY (user_authority) REFERENCES authority_table (authority_id),
-    award_day      DATE DEFAULT '1000-01-01' NOT NULL,
+    award_day      DATE        DEFAULT '1000-01-01'        NOT NULL,
     award_content  VARCHAR(50) DEFAULT '수상 내역이 존재하지 않습니다.' NOT NULL,
     award_people   VARCHAR(30) DEFAULT '수상자가 존재하지 않습니다.'   NOT NULL
 );
@@ -224,8 +248,12 @@ CREATE TABLE ledger_table
     FOREIGN KEY (user_authority) REFERENCES authority_table (authority_id),
     ledger_title       VARCHAR(30) DEFAULT '회비 내역이 존재하지 않습니다.' NOT NULL,
     ledger_useday      DATE        DEFAULT '1000-01-01'        NOT NULL,
+    ledger_field       TINYINT UNSIGNED NOT NULL,
+    FOREIGN KEY (ledger_field) REFERENCES field_table (field_id),
     ledger_minus       MEDIUMINT UNSIGNED NULL,
     ledger_plus        MEDIUMINT UNSIGNED NULL,
     ledger_sum         MEDIUMINT UNSIGNED NULL,
     ledger_evidence    VARCHAR(100) NULL
-); */
+);
+
+*/

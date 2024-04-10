d5a10b87cec8930b1897483aa2bd1e9f91e72d52
cprtmchzh <bin54511563@gmail.com>
2024-04-10 21:12:24 +0900

    Bug: 일부 Bean을 찾지 못하는 문제 해결

--- src/main/java/com/iKeeper/homepage/HomepageApplication.java
+++ src/main/java/com/iKeeper/homepage/HomepageApplication.java
@@ -3,7 +3,7 @@ package com.iKeeper.homepage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.springframework.security.crypto.password"})
 public class HomepageApplication {
 
 	public static void main(String[] args) {

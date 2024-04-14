e87a4df35175ebac8fc4475c2cdbd136a862f4be
cprtmchzh <bin54511563@gmail.com>
2024-04-14 23:24:07 +0900

    Fix: Spring Security 일부 설정 변경

--- src/main/java/com/iKeeper/homepage/global/config/SecurityConfig.java
+++ src/main/java/com/iKeeper/homepage/global/config/SecurityConfig.java
@@ -1,93 +1,46 @@
 package com.iKeeper.homepage.global.config;
 
-import com.iKeeper.homepage.domain.auth.service.UserSignInService;
 import com.iKeeper.homepage.global.jwt.handler.JwtAuthenticationFilter;
 import com.iKeeper.homepage.global.jwt.handler.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration(proxyBeanMethods = false)
+@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@ConditionalOnDefaultWebSecurity
-@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    UserSignInService userSignInService;
+public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    @Order(SecurityProperties.BASIC_AUTH_ORDER)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http
+
                 .httpBasic().disable()
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
                 .antMatchers("/", "/**").permitAll()
-                .antMatchers("/api/users/join", "/api/users/login").permitAll()
+                .antMatchers("/api/auths/join", "/api/auths/login").permitAll()
                 .antMatchers("/api/users/test").hasRole("USER")
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring()
-                .antMatchers("/css/**", "/js/**", "/img/**")
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api/users/join").permitAll()
-                .antMatchers("/").hasRole("USER")
-                .antMatchers("/admin").hasRole("ADMIN")
-                .anyRequest().authenticated(); // 페이지 및 권한은 추후 분배 예정
-
-        http
-                .formLogin()
-                .defaultSuccessUrl("/success")
-                .failureForwardUrl("/fail")
-                .usernameParameter("student")
-                .passwordParameter("password");
-
-        http
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/api/users/logout"))
-                .invalidateHttpSession(true);
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }

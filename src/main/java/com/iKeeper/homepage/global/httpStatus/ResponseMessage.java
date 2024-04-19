4b4af56066e78e456ec475a095e423e165c1f7bb
cprtmchzh <bin54511563@gmail.com>
2024-04-19 17:42:45 +0900

    Feat: 일정 및 인증 관련 상태 코드 추가

--- src/main/java/com/iKeeper/homepage/global/httpStatus/ResponseMessage.java
+++ src/main/java/com/iKeeper/homepage/global/httpStatus/ResponseMessage.java
@@ -2,9 +2,14 @@ package com.iKeeper.homepage.global.httpStatus;
 
 public class ResponseMessage {
 
+    //CALENDAR
     public static final String CALENDAR_POST = "일정 등록 성공";
-    public static final String CALENDAR_DATE_READ = "특정 일자 일정 조회 성공";
-    public static final String CALENDAR_ALL_READ = "전체 일정 조회 성공";
+    public static final String CALENDAR_READ_DATE = "특정 일자 일정 조회 성공";
+    public static final String CALENDAR_READ_ALL = "전체 일정 조회 성공";
     public static final String CALENDAR_PATCH = "일정 정보 수정 성공";
     public static final String CALENDAR_DELETE = "일정 삭제 성공";
+
+    //AUTH
+    public static final String AUTH_POST_JOIN = "회원 가입 성공";
+    public static final String AUTH_POST_LOGIN = "로그인 성공";
 }

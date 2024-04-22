c19d237d58333c4ffe10a8e96cd90a2a6cbc038c
cprtmchzh <bin54511563@gmail.com>
2024-04-22 17:39:29 +0900

    Feat: 마이페이지용 DB 칼럼 매핑

---
+++ src/main/java/com/iKeeper/homepage/domain/user/dao/MemberInfo.java
@@ -0,0 +1,37 @@
+package com.iKeeper.homepage.domain.user.dao;
+
+import com.iKeeper.homepage.domain.user.entity.Grade;
+import com.iKeeper.homepage.domain.user.entity.Major;
+import com.iKeeper.homepage.domain.user.entity.Status;
+import com.iKeeper.homepage.global.entity.Field;
+
+public interface MemberInfo {
+
+    String getStudentId();
+
+    String getName();
+
+    String getRole();
+
+    Field getField();
+
+    String getPnumber();
+
+    String getBirth();
+
+    String getEmail();
+
+    Status getStatus();
+
+    Grade getGrade();
+
+    Major getMajor1();
+
+    Major getMajor2();
+
+    Major getMajor3();
+
+    Major getMinor();
+
+    Boolean getWarning();
+}

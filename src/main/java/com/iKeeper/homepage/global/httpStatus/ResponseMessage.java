012d49e2570dad30d101282bb3def161bbae1721
cprtmchzh <bin54511563@gmail.com>
2024-04-22 17:41:31 +0900

    Feat: USER 관련 상태 코드 추가

--- src/main/java/com/iKeeper/homepage/global/httpStatus/ResponseMessage.java
+++ src/main/java/com/iKeeper/homepage/global/httpStatus/ResponseMessage.java
@@ -12,4 +12,9 @@ public class ResponseMessage {
     //AUTH
     public static final String AUTH_POST_JOIN = "회원 가입 성공";
     public static final String AUTH_POST_LOGIN = "로그인 성공";
+
+    //USER
+    public static final String USER_MYPAGE = "회원 정보 조회 성공";
+    public static final String USER_UPDATE_MYPAGE = "회원 정보 수정 성공";
+    public static final String USER_DELETE_ACCOUNT = "계정 탈퇴 성공";
 }

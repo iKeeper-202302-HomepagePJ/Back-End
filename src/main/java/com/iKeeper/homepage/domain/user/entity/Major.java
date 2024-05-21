e132c3584c77bf370a18eb773a97c1821d2f2c73
cprtmchzh <bin54511563@gmail.com>
2024-05-22 03:07:43 +0900

    Feat: 전공 생성 기능 추가

--- src/main/java/com/iKeeper/homepage/domain/user/entity/Major.java
+++ src/main/java/com/iKeeper/homepage/domain/user/entity/Major.java
@@ -1,5 +1,7 @@
 package com.iKeeper.homepage.domain.user.entity;
 
+import com.iKeeper.homepage.domain.user.dto.request.MajorRequest;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,4 +20,16 @@ public class Major {
 
     @Column(name = "major_name", nullable = false)
     private String name;
+
+    @Builder
+    public Major(String name) {
+        this.name = name;
+    }
+
+    public static Major createMajor(MajorRequest majorRequest) {
+        Major major = Major.builder()
+                .name(majorRequest.getName())
+                .build();
+        return major;
+    }
 }
